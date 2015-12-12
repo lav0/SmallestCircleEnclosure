f3d59d322830af31300fea642c45e0dc4d47e80f
AndrewOnMac <andreigenuine@yahoo.com>
2015-12-12 23:15:16 +0800

    Refactored direct methods.

--- SCE_Direct.py
+++ SCE_Direct.py
@@ -15,6 +15,19 @@ def check_and_replace(smallest_circle, circle, a_list_of_points):
     return smallest_circle, False;
 
 
+def check_smaller_circle(cur_small,
+                         lst,
+                         pivot_points,
+                         suspected_pivot,
+                         circle_builder,
+                         args):
+    circle = circle_builder(*args)
+    cur_small, reduced = check_and_replace(cur_small, circle, lst)
+    if reduced:
+        pivot_points = suspected_pivot
+    return cur_small, pivot_points
+
+
 @time_measure_decorator
 def find_smallest_circle_directly(a_list_of_points):
     smallest_circle = None;
@@ -22,26 +35,26 @@ def find_smallest_circle_directly(a_list_of_points):
     for p in a_list_of_points:
         for r in a_list_of_points:
             if p is not r:
-                circle = construct_circle_on_pair(r, p);
-                smallest_circle, reduced = check_and_replace(smallest_circle, circle, a_list_of_points);
-                if reduced:
-                    pivot_points = [p, r]
+                smallest_circle, pivot_points = check_smaller_circle(smallest_circle,
+                                                                     a_list_of_points,
+                                                                     pivot_points,
+                                                                     [p, r],
+                                                                     construct_circle_on_pair,
+                                                                     args=[r, p])
                 for q in a_list_of_points:
                     if q is not p and q is not r:
                         circle = construct_circle_on_triple(p, r, q);
                         smallest_circle, reduced = check_and_replace(smallest_circle, circle, a_list_of_points);
                         if reduced:
                             pivot_points = [p, r, q]
+                        smallest_circle, pivot_points = check_smaller_circle(smallest_circle,
+                                                                             a_list_of_points,
+                                                                             pivot_points,
+                                                                             [p, r, q],
+                                                                             construct_circle_on_triple,
+                                                                             args=[p, r, q])
 
-    return smallest_circle, pivot_points;
-
-
-def check_smaller_circle(cur_small, suspected_centre, point, lst, pivot_points, suspected_pivot):
-    circle = construct_circle_with_centre_and_point(suspected_centre, point)
-    cur_small, reduced = check_and_replace(cur_small, circle, lst)
-    if reduced:
-        pivot_points = suspected_pivot
-    return cur_small, pivot_points
+    return smallest_circle, pivot_points
 
 
 def find_constrained_centre(lst, line):
@@ -51,16 +64,19 @@ def find_constrained_centre(lst, line):
             projection = point_to_line_projection(p, line)
             assert line.is_point_on_line(projection)
             smallest_circle, pivot_points = check_smaller_circle(smallest_circle,
-                                                                 projection,
-                                                                 p, lst, pivot_points, [p])
+                                                                 lst, pivot_points, [p],
+                                                                 construct_circle_with_centre_and_point,
+                                                                 args=[projection, p])
             for q in lst:
                 if p is not q:
                     bisector = perpendicular_bisector(p, q)
                     intersection = bisector.intersection(line)
                     if intersection is not None:
                         smallest_circle, pivot_points = check_smaller_circle(smallest_circle,
-                                                                             intersection,
-                                                                             p, lst, pivot_points, [p, q])
+                                                                             lst, pivot_points, [p, q],
+                                                                             construct_circle_with_centre_and_point,
+                                                                             args=[intersection, p])
 
         return smallest_circle, pivot_points
+
 
